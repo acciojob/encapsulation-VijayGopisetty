ccb0affebc4149ea42ef731bc802672adf41b4aa
Vijay <dhruvakumar.gopisetty@gmail.com>
2024-02-02 20:51:30 +0530

    version 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj=new RWOnly();
+        obj.name=2;
+        System.out.println(obj.name);
+        //java: encap has private access in com.driver.RWOnly
+        obj.setname(5);
+        System.out.println(obj.getname());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int name;
+
+    public int getname() {
+        return name;
+    }
+
+    public void setname(int name) {
+        this.name = name;
+    }
 }
